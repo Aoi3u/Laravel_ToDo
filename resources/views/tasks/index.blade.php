3ee02e5095d21b4a1af4d4771ea97cdc48498a70
Aoi3u <miu.blue3@gmail.com>
2025-01-23 19:55:51 +0900

    デプロイ準備33

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ToDo</title>
-    <script src="{{ secure_asset('/js/script.js') }}" defer></script>
     @vite('resources/css/app.css')
 </head>
 
@@ -120,6 +119,40 @@
             }
             return false;
         }
+
+        window.addEventListener("load", function() {
+            // タイピングアニメーションの速度設定（ミリ秒）
+            const time = 150;
+
+            // .typing要素を処理
+            const elements = document.querySelectorAll(".typing");
+
+            elements.forEach((element) => {
+                // テキストを一文字ずつ分割して<span>タグで囲む
+                const text = element.textContent;
+                const spanText = Array.from(text)
+                    .map((char) =>
+                        char !== " " ?
+                        `<span style="display:none;">${char}</span>` :
+                        " "
+                    )
+                    .join("");
+
+                // 要素内のHTMLを更新
+                element.innerHTML = spanText;
+
+                // タイピングアニメーション
+                const spans = element.querySelectorAll("span");
+                spans.forEach((span, i) => {
+                    setTimeout(() => {
+                        span.style.display = "inline";
+                        span.style.opacity = 0;
+                        span.style.transition = `opacity ${time / 1000}s`;
+                        span.style.opacity = 1;
+                    }, time * i);
+                });
+            });
+        });
     </script>
 </body>
 
